b97a44a8e6f34559e2428dc81dc10f20483c95ef
dhruv2185 <dhruvghevariya2002@gmail.com>
2023-01-21 23:55:05 +0530

    frontend saved

--- BackEnd/model.py
+++ BackEnd/model.py
@@ -2,7 +2,7 @@ import numpy as np
 import tensorflow as tf
 from tensorflow import keras
 from recsys_utils import *
-#Load data
+# Load data
 X, W, b, num_movies, num_features, num_users = load_precalc_params_small()
 Y, R = load_ratings_small()
 
@@ -14,18 +14,20 @@ print("num_features", num_features)
 print("num_movies",   num_movies)
 print("num_users",    num_users)
 #  From the matrix, we can compute statistics like average rating.
-tsmean =  np.mean(Y[0, R[0, :].astype(bool)])
-print(f"Average rating for movie 1 : {tsmean:0.3f} / 5" )
+tsmean = np.mean(Y[0, R[0, :].astype(bool)])
+print(f"Average rating for movie 1 : {tsmean:0.3f} / 5")
 # Reduce the data set size so that this runs faster
 num_users_r = 4
-num_movies_r = 5 
+num_movies_r = 5
 num_features_r = 3
 
 X_r = X[:num_movies_r, :num_features_r]
-W_r = W[:num_users_r,  :num_features_r]
-b_r = b[0, :num_users_r].reshape(1,-1)
+W_r = W[:num_users_r, :num_features_r]
+b_r = b[0, :num_users_r].reshape(1, -1)
 Y_r = Y[:num_movies_r, :num_users_r]
 R_r = R[:num_movies_r, :num_users_r]
+
+
 def cofi_cost_func_v(X, W, b, Y, R, lambda_):
     """
     Returns the cost for the content-based filtering
@@ -41,44 +43,49 @@ def cofi_cost_func_v(X, W, b, Y, R, lambda_):
       J (float) : Cost
     """
     j = (tf.linalg.matmul(X, tf.transpose(W)) + b - Y)*R
-    J = 0.5 * tf.reduce_sum(j**2) + (lambda_/2) * (tf.reduce_sum(X**2) + tf.reduce_sum(W**2))
+    J = 0.5 * tf.reduce_sum(j**2) + (lambda_/2) * \
+        (tf.reduce_sum(X**2) + tf.reduce_sum(W**2))
     return J
+
+
 # Evaluate cost function
-J = cofi_cost_func_v(X_r, W_r, b_r, Y_r, R_r, 0);
+J = cofi_cost_func_v(X_r, W_r, b_r, Y_r, R_r, 0)
 print(f"Cost: {J:0.2f}")
 
-# Evaluate cost function with regularization 
-J = cofi_cost_func_v(X_r, W_r, b_r, Y_r, R_r, 1.5);
+# Evaluate cost function with regularization
+J = cofi_cost_func_v(X_r, W_r, b_r, Y_r, R_r, 1.5)
 print(f"Cost (with regularization): {J:0.2f}")
 movieList, movieList_df = load_Movie_List_pd()
 
-my_ratings = np.zeros(num_movies)          #  Initialize my ratings
+my_ratings = np.zeros(num_movies)  # Initialize my ratings
 
 # Check the file small_movie_list.csv for id of each movie in our dataset
 # For example, Toy Story 3 (2010) has ID 2700, so to rate it "5", you can set
-my_ratings[2700] = 5 
+my_ratings[2700] = 5
 
-#Or suppose you did not enjoy Persuasion (2007), you can set
+# Or suppose you did not enjoy Persuasion (2007), you can set
 my_ratings[2609] = 2
 
 # We have selected a few movies we liked / did not like and the ratings we
 # gave are as follows:
-my_ratings[929]  = 5   # Lord of the Rings: The Return of the King, The
-my_ratings[246]  = 5   # Shrek (2001)
+my_ratings[929] = 5   # Lord of the Rings: The Return of the King, The
+my_ratings[246] = 5   # Shrek (2001)
 my_ratings[2716] = 3   # Inception
 my_ratings[1150] = 5   # Incredibles, The (2004)
-my_ratings[382]  = 2   # Amelie (Fabuleux destin d'Amélie Poulain, Le)
-my_ratings[366]  = 5   # Harry Potter and the Sorcerer's Stone (a.k.a. Harry Potter and the Philosopher's Stone) (2001)
-my_ratings[622]  = 5   # Harry Potter and the Chamber of Secrets (2002)
-my_ratings[988]  = 3   # Eternal Sunshine of the Spotless Mind (2004)
+my_ratings[382] = 2   # Amelie (Fabuleux destin d'Amélie Poulain, Le)
+# Harry Potter and the Sorcerer's Stone (a.k.a. Harry Potter and the Philosopher's Stone) (2001)
+my_ratings[366] = 5
+my_ratings[622] = 5   # Harry Potter and the Chamber of Secrets (2002)
+my_ratings[988] = 3   # Eternal Sunshine of the Spotless Mind (2004)
 my_ratings[2925] = 1   # Louis Theroux: Law & Disorder (2008)
 my_ratings[2937] = 1   # Nothing to Declare (Rien à déclarer)
-my_ratings[793]  = 5   # Pirates of the Caribbean: The Curse of the Black Pearl (2003)
+# Pirates of the Caribbean: The Curse of the Black Pearl (2003)
+my_ratings[793] = 5
 my_rated = [i for i in range(len(my_ratings)) if my_ratings[i] > 0]
 
 print('\nNew user ratings:\n')
 for i in range(len(my_ratings)):
-    if my_ratings[i] > 0 :
+    if my_ratings[i] > 0:
         print(f'Rated {my_ratings[i]} for  {movieList_df.loc[i,"title"]}')
 # Reload ratings
 Y, R = load_ratings_small()
@@ -96,10 +103,13 @@ num_movies, num_users = Y.shape
 num_features = 100
 
 # Set Initial Parameters (W, X), use tf.Variable to track these variables
-tf.random.set_seed(1234) # for consistent results
-W = tf.Variable(tf.random.normal((num_users,  num_features),dtype=tf.float64),  name='W')
-X = tf.Variable(tf.random.normal((num_movies, num_features),dtype=tf.float64),  name='X')
-b = tf.Variable(tf.random.normal((1,          num_users),   dtype=tf.float64),  name='b')
+tf.random.set_seed(1234)  # for consistent results
+W = tf.Variable(tf.random.normal(
+    (num_users,  num_features), dtype=tf.float64),  name='W')
+X = tf.Variable(tf.random.normal(
+    (num_movies, num_features), dtype=tf.float64),  name='X')
+b = tf.Variable(tf.random.normal(
+    (1,          num_users),   dtype=tf.float64),  name='b')
 
 # Instantiate an optimizer.
 optimizer = keras.optimizers.Adam(learning_rate=1e-1)
@@ -107,7 +117,7 @@ iterations = 200
 lambda_ = 1
 for iter in range(iterations):
     # Use TensorFlow’s GradientTape
-    # to record the operations used to compute the cost 
+    # to record the operations used to compute the cost
     with tf.GradientTape() as tape:
 
         # Compute the cost (forward pass included in cost)
@@ -115,11 +125,11 @@ for iter in range(iterations):
 
     # Use the gradient tape to automatically retrieve
     # the gradients of the trainable variables with respect to the loss
-    grads = tape.gradient( cost_value, [X,W,b] )
+    grads = tape.gradient(cost_value, [X, W, b])
 
     # Run one step of gradient descent by updating
     # the value of the variables to minimize the loss.
-    optimizer.apply_gradients( zip(grads, [X,W,b]) )
+    optimizer.apply_gradients(zip(grads, [X, W, b]))
 
     # Log periodically.
     if iter % 20 == 0:
@@ -127,11 +137,11 @@ for iter in range(iterations):
 # Make a prediction using trained weights and biases
 p = np.matmul(X.numpy(), np.transpose(W.numpy())) + b.numpy()
 
-#restore the mean
+# restore the mean
 pm = p + Ymean
 
-my_predictions = pm[:,0]
-movies_to_send= []
+my_predictions = pm[:, 0]
+movies_to_send = []
 
 # sort predictions
 ix = tf.argsort(my_predictions, direction='DESCENDING')
--- sasta-moviesverse/src/components/HomeBanner.js
+++ sasta-moviesverse/src/components/HomeBanner.js
@@ -1,6 +1,6 @@
 import React from 'react';
 const HomeBanner = () => {
-  return(
+  return (
     <div className="home-banner">
       <div className="our-story">
         <h1 className="our-story-card-title" data-uia="hero-title">Unlimited movies, TV shows and more.</h1>
@@ -12,7 +12,7 @@ const HomeBanner = () => {
         </div>
       </div>
       <div className="shadow"></div>
-      {/* <img className="concord-img vlv-creative" src="https://assets.nflxext.com/ffe/siteui/vlv3/6e32b96a-d4be-4e44-a19b-1bd2d2279b51/ee068656-14b9-4821-89b4-53b4937d9f1c/IN-en-20220516-popsignuptwoweeks-perspective_alpha_website_small.jpg"  alt=""></img> */}
+      <img className="concord-img vlv-creative" src="https://assets.nflxext.com/ffe/siteui/vlv3/6e32b96a-d4be-4e44-a19b-1bd2d2279b51/ee068656-14b9-4821-89b4-53b4937d9f1c/IN-en-20220516-popsignuptwoweeks-perspective_alpha_website_small.jpg" alt=""></img>
     </div>
   )
 }
