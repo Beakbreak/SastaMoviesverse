cfc070dfdf02b9ddc38bb109991e58e75144a873
TilteD24 <21bcs080@iiitdmj.ac.in>
2023-01-22 02:03:08 +0530

    backend modified

--- BackEnd/app.py
+++ BackEnd/app.py
@@ -6,7 +6,8 @@ from wtforms import StringField, PasswordField, SubmitField
 from wtforms.validators import InputRequired, Length, ValidationError
 from flask_bcrypt import Bcrypt
 from flask_mysqldb import MySQL
- 
+import MySQLdb.cursors
+
 app = Flask(__name__)
  
 app.config['MYSQL_HOST'] = 'localhost'
@@ -17,35 +18,45 @@ app.config['MYSQL_DB'] = 'moviesverse'
 mysql = MySQL(app)
 bcrypt = Bcrypt(app)
 
-cursor = mysql.connection.cursor()
-
 @app.route('/')
 def home() :
     return "Hello world"
 
 @app.route('/login', methods = ['POST'])
 def login():
-    email = request.form['email']
-    pwd = request.form['password']
+    cursor = mysql.connection.cursor()
 
-    query = "SELECT password FROM users WHERE email = {email}"
+    content = request.json
+    email = content['email']
+    pwd = content['password']
+
+    query = "SELECT * FROM registered_users WHERE email = '{email}' AND password='{pwd}'"
 
     cursor.execute(query)
+    mysql.connection.commit()
+    cursor.close()
 
-    data = cursor.fetchall()
-
-    print(data)
-
-@app.route('/register', methods = ['POST'])
+@app.route('/register', methods = ['GET','POST'])
 def register():
-    name = request.form['username']
-    email = request.form['email']
-    pwd = request.form['password']
+    cursor = mysql.connection.cursor()
+    content = request.json
+
+    cursor.execute('SELECT * FROM registered_users')
+    count = len(cursor.fetchall())
+    email = content['email']
+    pwd = content['password']
     
-    query = "INSERT INTO users VALUES (%s, %s, %s)"
-    data = (name, email, pwd)
+    query = "INSERT INTO registered_users VALUES (%s, %s, %s)"
+    data = ( count,email, pwd)
 
     cursor.execute(query, data)
+    mysql.connection.commit()
+    cursor.close()
+
+
+@app.route('/predict', methods = ['POST'])
+def predict() : 
+    movie_ratings = []
 
 
 if __name__ == "__main__":
