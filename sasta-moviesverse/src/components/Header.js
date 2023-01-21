c6e05a5c5b5e75cdd414a0a8c7e894d2f123ef7b
dhruv2185 <dhruvghevariya2002@gmail.com>
2023-01-21 18:22:07 +0530

    renamed

--- sasta-moviesverse/src/components/Header.js
+++ sasta-moviesverse/src/components/Header.js
@@ -1,4 +1,5 @@
 import { useNavigate, Link } from "react-router-dom";
+import React from 'react';
 
 const Header = () => {
   const navigate = useNavigate();
@@ -13,7 +14,7 @@ const Header = () => {
       <nav className="navbar navbar-expand-md navbar-dark">
         <div className="container-fluid">
           <Link className="navbar-brand" to="/">
-            <img className="nav__logo" src="https://www.freepnglogos.com/uploads/netflix-logo-0.png" alt="" />
+            {/* <img className="nav__logo" src="https://www.freepnglogos.com/uploads/netflix-logo-0.png" alt="" /> */}
           </Link>
           
           <div className="navbar">
