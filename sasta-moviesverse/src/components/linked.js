02217483d7012f9781b04d7a6253fc60b1e6a916
dhruv2185 <dhruvghevariya2002@gmail.com>
2023-01-22 12:13:21 +0530

    rating UI added

---
+++ sasta-moviesverse/src/components/linked.js
@@ -0,0 +1,8 @@
+import React from "react";
+const linked = (props) => {
+    return (<>
+        <h1>props.moviedata</h1>
+        <Rating name="rate-movie" value={null} > Rate this movie!</Rating>
+    </>)
+}
+export default linked;
