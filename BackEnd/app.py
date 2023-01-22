807c1d37d3e627a36cb78625ad16c2d0e4888a56
dhruv2185 <dhruvghevariya2002@gmail.com>
2023-01-22 15:54:32 +0530

    jsdbcjsbcjksd

--- BackEnd/app.py
+++ BackEnd/app.py
@@ -30,19 +30,14 @@ def rating_insert():
     content = request.json
 
     email = content['email']
-    cursor.execute(f'SELECT u_id FROM registered_users WHERE email=')
-
     movie_id = content['movie_id']
     rating = content['rating']
-
-    l = cursor.fetchall()
-    print(l)
-    u_id = l[0][0]
-    query = f"INSERT INTO new_ratings VALUES ({u_id},{movie_id},{rating})"
-    data = (u_id, movie_id, rating)
+    query = 'INSERT INTO new_ratings VALUES ((SELECT u_id FROM registered_users WHERE email=%s),%s,%s)'
+    data = (email, movie_id, rating)
 
     cursor.execute(query, data)
     mysql.connection.commit()
+    return 'got it'
 
 
 @app.route('/login', methods=['POST'])
@@ -152,29 +147,6 @@ def predict(res):
 
     for row in res:
         my_ratings[row[0]] = row[1]
-
-    # Check the file small_movie_list.csv for id of each movie in our dataset
-    # For example, Toy Story 3 (2010) has ID 2700, so to rate it "5", you can set
-    my_ratings[2700] = 5
-
-    # Or suppose you did not enjoy Persuasion (2007), you can set
-    my_ratings[2609] = 2
-
-    # We have selected a few movies we liked / did not like and the ratings we
-    # gave are as follows:
-    # my_ratings[929] = 5   # Lord of the Rings: The Return of the King, The
-    # my_ratings[246] = 5   # Shrek (2001)
-    # my_ratings[2716] = 3   # Inception
-    # my_ratings[1150] = 5   # Incredibles, The (2004)
-    # my_ratings[382] = 2   # Amelie (Fabuleux destin d'Amélie Poulain, Le)
-    # Harry Potter and the Sorcerer's Stone (a.k.a. Harry Potter and the Philosopher's Stone) (2001)
-    # my_ratings[366] = 5
-    # my_ratings[622] = 5   # Harry Potter and the Chamber of Secrets (2002)
-    # my_ratings[988] = 3   # Eternal Sunshine of the Spotless Mind (2004)
-    # my_ratings[2925] = 1   # Louis Theroux: Law & Disorder (2008)
-    # my_ratings[2937] = 1   # Nothing to Declare (Rien à déclarer)
-    # Pirates of the Caribbean: The Curse of the Black Pearl (2003)
-    # my_ratings[793] = 5
     my_rated = [i for i in range(len(my_ratings)) if my_ratings[i] > 0]
 
     print('\nNew user ratings:\n')
