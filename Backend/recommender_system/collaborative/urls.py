ef66be4c4e9f5487d6a6e8e4ab8f06d54943b7fd
beakbreak <namanbhagat1414@gmail.com>
2023-03-31 18:55:31 +0530

    model commit

--- Backend/recommender_system/collaborative/urls.py
+++ Backend/recommender_system/collaborative/urls.py
@@ -3,7 +3,10 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    # path('home/')
+    # path('insert/', views.insert),
+    path('delete/', views.delete_all),
     path('ratings/<str:pk>/', views.get_ratings),
-    path('trial/<str:pk>/',views.trial)
+    path('trial/<str:pk>/',views.trial),
+    path('train/<str:pk>/',views.train),
+    path('suggestions/',views.create_suggestions)
 ]
