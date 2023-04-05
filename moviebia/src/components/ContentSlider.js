ae0d13a427c7559ea920b65b056636263158d098
Dhruv <dhruv2185>
2023-04-05 08:43:16 +0530

    finished banner

--- moviebia/src/components/ContentSlider.js
+++ moviebia/src/components/ContentSlider.js
@@ -1,7 +1,25 @@
 import classes from './ContentSlider.module.scss';
-const ContentSlider = () => {
-    return (<>
+import MoviePoster from './MoviePoster';
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import Slider from 'react-slick';
+const ContentSlider = (props) => {
+    const settings = {
+        dots: true,
+        infinite: true,
+        speed: 500,
+        slidesToShow: 6
 
+        ,
+        slidesToScroll: 3
+    };
+
+    return (<>
+        <div >
+            <h2>Suggested For You</h2>
+            <Slider {...settings} className={classes.slider}>
+                <div className={classes.posterDiv}><MoviePoster /></div><div className={classes.posterDiv}><MoviePoster /></div><div className={classes.posterDiv}><MoviePoster /></div><div className={classes.posterDiv}><MoviePoster /></div><div className={classes.posterDiv}><MoviePoster /></div><div className={classes.posterDiv}><MoviePoster /></div><div className={classes.posterDiv}><MoviePoster /></div><div className={classes.posterDiv}><MoviePoster /></div></Slider>
+        </div>
     </>);
 }
 export default ContentSlider;
