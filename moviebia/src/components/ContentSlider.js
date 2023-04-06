58a62bb07e22b5b0e6f0c330bef42d41315d4c98
Dhruv <dhruv2185>
2023-04-06 20:00:42 +0530

    slider edited

--- moviebia/src/components/ContentSlider.js
+++ moviebia/src/components/ContentSlider.js
@@ -24,18 +24,24 @@ function SamplePrevArrow(props) {
 }
 const ContentSlider = (props) => {
     const settings = {
-        dots: true,
+        dots: false,
         infinite: true,
         speed: 500,
         slidesToShow: 6,
         slidesToScroll: 3,
         nextArrow: <SampleNextArrow />,
         prevArrow: <SamplePrevArrow />,
+        variableWidth: true,
+        adaptiveHeight: true
     };
 
     return (<>
         <div >
-            <h2 className={classes.movType}>{props.title}</h2>
+            <h2 className={classes.movType}>{props.title}<p className={classes.viewall}>View All<div className={classes["arrow-container"]}>
+                <div className={classes.arrowt}></div>
+                <div className={classes.arrowt}></div>
+                <div className={classes.arrowt}></div>
+            </div></p></h2>
             <Slider {...settings} className={classes.slider}>
                 <div className={classes.posterDiv}><MoviePoster /></div><div className={classes.posterDiv}><MoviePoster /></div><div className={classes.posterDiv}><MoviePoster /></div><div className={classes.posterDiv}><MoviePoster /></div><div className={classes.posterDiv}><MoviePoster /></div><div className={classes.posterDiv}><MoviePoster /></div><div className={classes.posterDiv}><MoviePoster /></div><div className={classes.posterDiv}><MoviePoster /></div></Slider>
         </div >
--- moviebia/src/components/Dashboard.js
+++ moviebia/src/components/Dashboard.js
@@ -1,15 +1,12 @@
 
 import Banner from './Banner';
 import Navmenu from './Navmenu';
-import classes from './Dashboard.module.css';
+import classes from './Dashboard.module.scss';
 import ContentSlider from './ContentSlider';
 const Dashboard = () => {
     return (<>
         <div className={classes["background-container"]}>
-            <img className={classes.moon} src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/1231630/moon2.png" alt="" />
-            <div className={classes.stars}></div>
-            <div className={classes.twinkling}></div>
-            <div className={classes.clouds}></div></div>
+        </div>
         <Navmenu />
         <Banner />
 
